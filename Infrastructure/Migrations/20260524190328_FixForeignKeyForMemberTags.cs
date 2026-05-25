2b8e1f4953c5d0da820ce0cceb3689e48a6a279d
SkyReed <itzskyreed@gmail.com>
2026-05-25 18:27:49 +0500

    fix: фикс, что Tag из PlayerPerformances не считался внешним ключем на Tag из ClanMembers

---
+++ Infrastructure/Migrations/20260524190328_FixForeignKeyForMemberTags.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixForeignKeyForMemberTags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_clan_war_player_performances_PlayerTag",
+                table: "clan_war_player_performances",
+                column: "PlayerTag");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_clan_war_player_performances_clan_members_PlayerTag",
+                table: "clan_war_player_performances",
+                column: "PlayerTag",
+                principalTable: "clan_members",
+                principalColumn: "Tag");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_clan_war_player_performances_clan_members_PlayerTag",
+                table: "clan_war_player_performances");
+
+            migrationBuilder.DropIndex(
+                name: "IX_clan_war_player_performances_PlayerTag",
+                table: "clan_war_player_performances");
+        }
+    }
+}
