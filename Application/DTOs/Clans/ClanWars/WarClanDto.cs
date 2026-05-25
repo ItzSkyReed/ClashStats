fff3eb534ecdea3f19b1b7ff1c1176378b477a18
SkyReed <itzskyreed@gmail.com>
2026-05-25 18:26:49 +0500

    style

--- Application/DTOs/Clans/ClanWars/WarClanDto.cs
+++ Application/DTOs/Clans/ClanWars/WarClanDto.cs
@@ -2,14 +2,14 @@
 
 public record WarClanDto
 {
-    public required string Tag{ get; init; }
-    public required string Name{ get; init; }
-    public required BadgeUrlsDto BadgeUrls{ get; init; }
-    public required int ClanLevel{ get; init; }
-    public required int Attacks{ get; init; }
-    public required int Stars{ get; init; }
-    public required float DestructionPercentage{ get; init; }
-    public int? ExpEarned{ get; init; }
+    public required string Tag { get; init; }
+    public required string Name { get; init; }
+    public required BadgeUrlsDto BadgeUrls { get; init; }
+    public required int ClanLevel { get; init; }
+    public required int Attacks { get; init; }
+    public required int Stars { get; init; }
+    public required float DestructionPercentage { get; init; }
+    public int? ExpEarned { get; init; }
 
-    public List<ClanWarMemberDto>? Members{ get; init; }
+    public List<ClanWarMemberDto>? Members { get; init; }
 }
