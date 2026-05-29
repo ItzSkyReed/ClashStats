2422731a11eda247da8240a8fe214a6cd9b5789c
SkyReed <itzskyreed@gmail.com>
2026-05-30 01:26:53 +0500

    fix: исправлены названия стейтов ЛВК

--- Domain/Constants/ClanWarLeagueState.cs
+++ Domain/Constants/ClanWarLeagueState.cs
@@ -7,11 +7,13 @@ namespace Domain.Constants;
 [JsonConverter(typeof(SmartEnumValueConverter<ClanWarLeagueState, string>))]
 public sealed class ClanWarLeagueState : SmartEnum<ClanWarLeagueState, string>
 {
-    private ClanWarLeagueState(string name, string value) : base(name, value) { }
+    public static readonly ClanWarLeagueState GroupNotFound = new(nameof(GroupNotFound), "groupNotFound");
+    public static readonly ClanWarLeagueState NotInWar = new(nameof(NotInWar), "notInWar");
+    public static readonly ClanWarLeagueState Preparation = new(nameof(Preparation), "preparation");
+    public static readonly ClanWarLeagueState InWar = new(nameof(InWar), "inWar");
+    public static readonly ClanWarLeagueState WarEnded = new(nameof(WarEnded), "warEnded");
 
-    public static readonly ClanWarLeagueState GroupNotFound = new(nameof(GroupNotFound), "group_not_found");
-    public static readonly ClanWarLeagueState NotInWar = new(nameof(NotInWar),"not_in_war");
-    public static readonly ClanWarLeagueState Preparation = new(nameof(Preparation),"preparation");
-    public static readonly ClanWarLeagueState War = new(nameof(War),"war");
-    public static readonly ClanWarLeagueState Ended = new(nameof(Ended),"ended");
+    private ClanWarLeagueState(string name, string value) : base(name, value)
+    {
+    }
 }
