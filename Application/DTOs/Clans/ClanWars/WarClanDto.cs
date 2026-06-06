63c68b43051c7337ab94daf358c7190ddfae3b16
SkyReed <itzskyreed@gmail.com>
2026-06-07 01:00:01 +0500

    feat: Attacks в WarClanDto сделан не обязательным

--- Application/DTOs/Clans/ClanWars/WarClanDto.cs
+++ Application/DTOs/Clans/ClanWars/WarClanDto.cs
@@ -6,7 +6,7 @@ public record WarClanDto
     public string? Name { get; init; }
     public required BadgeUrlsDto BadgeUrls { get; init; }
     public required int ClanLevel { get; init; }
-    public required int Attacks { get; init; }
+    public int? Attacks { get; init; }
     public required int Stars { get; init; }
     public required float DestructionPercentage { get; init; }
     public int? ExpEarned { get; init; }
