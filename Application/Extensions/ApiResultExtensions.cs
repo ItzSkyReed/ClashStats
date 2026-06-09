133f988409ca92e344c8f168c72f21520019d7c3
SkyReed <itzskyreed@gmail.com>
2026-06-09 17:36:40 +0500

    feat: улучшен метод лога обработки ошибок от COC

--- Application/Extensions/ApiResultExtensions.cs
+++ Application/Extensions/ApiResultExtensions.cs
@@ -1,27 +1,35 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Net;
+using System.Runtime.CompilerServices;
 using Application.Interfaces;
 using Microsoft.Extensions.Logging;
 
 namespace Application.Extensions;
 
-public static class ApiResultExtensions
+public static partial class ApiResultExtensions
 {
     public static async Task<T?> UnwrapOrLogAsync<T>(
         this Task<IApiResult<T>> task,
         ILogger logger,
         [CallerMemberName] string method = "",
         [CallerFilePath] string file = "",
-        [CallerLineNumber] int line = 0
-    )
-
+        [CallerLineNumber] int line = 0)
     {
         var result = await task;
 
         if (result.IsSuccess)
             return result.Data;
 
-        logger.LogError("Api error {file} -> {method} (line {line}): {ErrorMessage}", file, method, line, result.Error?.Message);
+        var errorMessage = result.Error?.Message ?? "No specific error message provided by the API.";
+
+        var fileName = Path.GetFileName(file);
+
+        logger.LogApiError(fileName, method, line, result.StatusCode, (int)result.StatusCode, errorMessage);
 
         return default;
     }
+
+    [LoggerMessage(LogLevel.Error,
+        "Api error {FileName} -> {Method} (line {Line}). HTTP Status: {StatusCode} ({StatusCodeInt}). Message: {ErrorMessage}")]
+    static partial void LogApiError(this ILogger logger, string fileName, string method, int line, HttpStatusCode statusCode, int statusCodeInt,
+        string errorMessage);
 }
