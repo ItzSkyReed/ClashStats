cad6ad7c3675a09d3509ed5e380613d8cb379fe7
SkyReed <itzskyreed@gmail.com>
2026-06-13 02:01:15 +0500

    fix: правильное значение строки энума для notMember клана

--- Domain/Constants/ClanRole.cs
+++ Domain/Constants/ClanRole.cs
@@ -7,11 +7,13 @@ namespace Domain.Constants;
 [JsonConverter(typeof(SmartEnumValueConverter<ClanRole, string>))]
 public sealed class ClanRole : SmartEnum<ClanRole, string>
 {
-    private ClanRole(string name, string value) : base(name, value) { }
-
-    public static readonly ClanRole NotMember = new(nameof(NotMember), "not_member");
+    public static readonly ClanRole NotMember = new(nameof(NotMember), "notMember");
     public static readonly ClanRole Member = new(nameof(Member), "member");
     public static readonly ClanRole Leader = new(nameof(Leader), "leader");
     public static readonly ClanRole CoLeader = new(nameof(CoLeader), "coLeader");
     public static readonly ClanRole Admin = new(nameof(Admin), "admin");
+
+    private ClanRole(string name, string value) : base(name, value)
+    {
+    }
 }
