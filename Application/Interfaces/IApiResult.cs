bda0ff8c6745afd6feaed0dea14311483785bd22
SkyReed <itzskyreed@gmail.com>
2026-06-16 17:15:26 +0500

    perf: добавлены подсказки компилятору для ApiResult структуры

--- Application/Interfaces/IApiResult.cs
+++ Application/Interfaces/IApiResult.cs
@@ -1,16 +1,22 @@
-﻿using System.Net;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net;
 using Application.ClashOfClansModels.Common;
 
 namespace Application.Interfaces;
 
 public interface IApiResult
 {
+    [MemberNotNullWhen(false, nameof(Error))]
     bool IsSuccess { get; }
+
     ClientErrorDto? Error { get; }
     HttpStatusCode StatusCode { get; }
 }
 
 public interface IApiResult<out T> : IApiResult
 {
+    [MemberNotNullWhen(true, nameof(Data))]
+    new bool IsSuccess { get; }
+
     T? Data { get; }
 }
